e1728acac8f8a306b0dd954d9907b0a70e48e78f
Subrata Mondal <subratamondal1029@outlook.com>
2025-01-24 11:43:08 +0530

    add condition for error handling

--- js_files/script.js
+++ js_files/script.js
@@ -26,32 +26,29 @@ document.addEventListener("DOMContentLoaded", () => {
   animateOnScroll(); // Initial check on page load
 });
 
+// JavaScript for Carousel
+if (location.pathname.includes("index.html")) {
+  const images = document.querySelectorAll(".carousel img");
+  let currentIndex = 0;
 
+  function showImage(index) {
+    images.forEach((img, i) => {
+      img.classList.toggle("active", i === index);
+    });
+  }
 
-
-
-// JavaScript for Carousel 
-
-const images = document.querySelectorAll('.carousel img');
-let currentIndex = 0;
-
-function showImage(index) {
-  images.forEach((img, i) => {
-    img.classList.toggle('active', i === index);
+  document.getElementById("prev").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
   });
+
+  document.getElementById("next").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  });
+
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }, 3000); // Change interval to 2 seconds
 }
-
-document.getElementById('prev').addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  showImage(currentIndex);
-});
-
-document.getElementById('next').addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
-});
-
-setInterval(() => {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
-}, 3000); // Change interval to 2 seconds
