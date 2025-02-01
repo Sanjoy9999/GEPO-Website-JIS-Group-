d831ce62b7a77ef3aae89fc2895557e71501971d
Subrata Mondal <subratamondal1029@outlook.com>
2025-02-01 12:41:09 +0530

    contact form submit complete

--- js_files/contact.js
+++ js_files/contact.js
@@ -2,11 +2,27 @@
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 const form = document.querySelector("#contact-container form");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
 
-  console.log(data);
+  console.log(formData);
   // make api request
+  try {
+    form.querySelector("#submitBtn").disabled = true;
+    // /contact-application POST
+    const response = await axios.post(
+      "http://localhost:8080/api/contact-application",
+      formData,
+      { withCredentials: true }
+    );
+
+    const data = response.data.data;
+    console.log(data);
+    alert("Your message has been sent successfully!");
+    form.querySelector("#submitBtn").disabled = false;
+  } catch (error) {
+    console.error(error);
+    form.querySelector("#submitBtn").disabled = false;
+  }
 });
