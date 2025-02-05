ffafdbc5b12e11a2bbfa2d2409f6533f66a1daf7
Subrata Mondal <subratamondal1029@outlook.com>
2025-02-05 15:29:10 +0530

    remove params from upcomming events

--- backend/src/routes/upcomingEventRoutes.php
+++ backend/src/routes/upcomingEventRoutes.php
@@ -7,5 +7,5 @@ use Middleware\VerifyUser;
 
 $router->get('/upcoming-events', [UpcomingEventsController::class, 'getAllUpcomingEvents']);
 $router->post('/upcoming-events', [UpcomingEventsController::class, 'createUpcomingEvent'], [VerifyUser::class]);
-$router->patch('/upcoming-events/:id', [UpcomingEventsController::class, 'updateUpcomingEvent'], [VerifyUser::class]);
-$router->delete('/upcoming-events/:id', [UpcomingEventsController::class, 'deleteUpcomingEvent'], [VerifyUser::class]);
+$router->patch('/upcoming-events', [UpcomingEventsController::class, 'updateUpcomingEvent'], [VerifyUser::class]);
+$router->delete('/upcoming-events', [UpcomingEventsController::class, 'deleteUpcomingEvent'], [VerifyUser::class]);
